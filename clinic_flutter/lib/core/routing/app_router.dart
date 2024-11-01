39998bb0ca440d3f56c1f186961c69e703ce8eaf
Bilal Hairab <bilal.hairab@gmail.com>
2024-11-01 22:06:21 +0400

    Basic App

---
+++ clinic_flutter/lib/core/routing/app_router.dart
@@ -0,0 +1,23 @@
+import 'package:clinic_flutter/core/routing/routes.dart';
+import 'package:flutter/material.dart';
+
+import '../../features/onboarding/onboarding_screen.dart';
+import '../../features/login/ui/widgets/login_screen.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => OnBoardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => LoginScreen());
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(
+                    child: Text("No route defined for ${settings.name}"),
+                  ),
+                ));
+    }
+  }
+}
