10d174fabbc782c9e4f7d300323eeb324f0fd287
Bilal Hairab <bilal.hairab@gmail.com>
2024-11-08 19:18:31 +0400

    Fix texts not showing in release flavors

--- clinic_flutter/lib/main_development.dart
+++ clinic_flutter/lib/main_development.dart
@@ -1,11 +1,13 @@
 import 'package:clinic_flutter/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/di/dependency_injection.dart';
 import 'doc_app.dart';
 
-void main() {
+void main() async {
   setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
